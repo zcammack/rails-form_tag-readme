b30d01c378ab9884e39d74f8d742e629836e4027
zcammack <zack.cammack@gmail.com>
2020-12-19 18:21:57 -0600

    commit tree 3a6aafd68e0990cce4d7a333c327ff2675fa8998

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,13 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def new
+    
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
 end
